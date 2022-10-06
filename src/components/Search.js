2aa06f718d1d9bd8da5643bdd8e6b5f58a431a26
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-06 12:14:23 +0300

    Add styling to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,55 @@
 import React from "react";
 
 function Search() {
- const searchBar = {
+  const searchBar = {
     display: "flex",
     justifyContent: "right",
- }
+  };
 
+  const input = {
+    background: "rgba(255, 255, 255, 0.2)",
+    border: "1px solid rgba(255, 255, 255, 0.5)",
+    color: "#D9D9D9",
+    margin: "2px",
+    padding: "5px",
+    marginBottom: "30px",
+    fontSize: "15px",
+  };
 
- function handleSearch(){
+  const options = {
+    background: "rgba(255, 255, 255, 0.2)",
+    border: "1px solid rgba(255, 255, 255, 0.5)",
+    color: "black",
+    fontSize: "15px",
+  };
+
+  function handleSearch() {
     console.log("searching");
- }
+  }
 
   return (
     <div style={searchBar}>
-      <input type="text" name="search" placeholder="keyword/title" />
-      <select name="" id="">
-        <option value="movie">Movie</option>
-        <option value="series">Series</option>
-        <option value="episode">Documentary</option>
+      <input
+        type="text"
+        name="search"
+        placeholder="keyword/title"
+        style={input}
+      />
+      <select name="categories" id="categories" style={input}>
+        <option value="" style={options}></option>
+        <option value="movie" style={options}>
+          Movie
+        </option>
+        <option value="series" style={options}>
+          Series
+        </option>
+        <option value="episode" style={options}>
+          Documentary
+        </option>
       </select>
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch} style={input}>
+        Search
+      </button>
     </div>
   );
 }
