ad51e0b0f332ced4ef8c7649151d5418b14afa04
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-07 07:49:18 +0300

    pass fetched data as props to children

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import { Route, Switch } from "react-router-dom";
-import './assets/App.css';
-import Home from './Home';
+import "./assets/App.css";
+import Home from "./Home";
 import Movies from "./Movies";
 import Series from "./Series";
 import Documentaries from "./Documentaries";
 import WatchList from "./WatchList";
-import Login from './Login';
-import SignUp from './SignUp';
-
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/d")
+      .then((res) => res.json())
+      .then((data) => {
+        setFilms(data);
+      });
+  }, []);
+
   return (
-    <div >
+    <div>
       <Switch>
         <Route exact path="/movies">
-          <Movies />
+          <Movies films={films} />
         </Route>
         <Route exact path="/series">
-          <Series />
+          <Series films={films} />
         </Route>
         <Route exact path="/documentaries">
           <Documentaries />
@@ -33,7 +42,7 @@ function App() {
           <SignUp />
         </Route>
         <Route exact path="/">
-          <Home />
+          <Home films={films} />
         </Route>
         <Route path="*">
           <h1>404 not found</h1>
