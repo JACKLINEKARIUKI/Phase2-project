8307e661500506d9d5ab53c6598a4e931bb4742d
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-07 21:52:12 +0300

    add state  variables for  watch list component

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import FilmList from "./FilmList";
 
-function WatchList({toWatch}) {
+const watchListStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(5, 1fr)",
+  gridGap: "1.9em",
+};
 
-    const watchListStyle = {
-      display: "grid",
-      gridTemplateColumns: "repeat(5, 1fr)",
-      gridGap: "1.9em",
-    };
+function WatchList() {
+  const [watchList, setWatchList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/w")
+      .then((res) => res.json())
+      .then((data) => {
+        setWatchList(data);
+      });
+  }, []);
 
   return (
     <div>
       <Header />
-      <FilmList watchList={toWatch} listStyle={watchListStyle}/>
+      <FilmList films={watchList} listStyle={watchListStyle} />
       <Footer />
     </div>
   );
