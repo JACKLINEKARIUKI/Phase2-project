966a90d7102b0e1de0a02ea106707a81e6686af5
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-07 09:45:20 +0300

    add context provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom"
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import FilmsProvider from "./Context/Films";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FilmsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FilmsProvider>
   </React.StrictMode>
 );
-
