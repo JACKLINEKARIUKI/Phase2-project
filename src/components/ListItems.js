77d5c0efc1858facbe2b1265bb415df096e1b0c3
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-07 07:47:12 +0300

    add code to display fetched data

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,14 +1,20 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
-function ListItems({ items }) {
-    return (
-        <div>
-        {/* {items.map((item) => (
-            <MovieCard key={item.id} />
-        ))} */}
-        </div>
-    );
-    }
+function ListItems({ films }) {
+  const listItems = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "2em",
+  };
+
+  return (
+    <div style={listItems}>
+      {films.map((film) => (
+        <MovieCard key={film.id} film={film} />
+      ))}
+    </div>
+  );
+}
 
 export default ListItems;
